ba368dc2cc31799518342d4d2e1365eb91fee1b2
Nick Belzer <me@nickbelzer.me>
2020-04-10 17:36:05 +0200

    Generalise table data loading

--- load_data.py
+++ load_data.py
@@ -5,6 +5,6 @@ import json
 def read_tables(filename="example.json") -> [Table]:
     """Reads a json file in the data directory and outputs the tables parsed
     from it."""
-    with open('data/{filename}'.format(filename=filename), 'r') as file:
+    with open('data/tables/{filename}'.format(filename=filename), 'r') as file:
         data = json.loads(file.read())
         return [Table(id=table, **data[table]) for table in data]
