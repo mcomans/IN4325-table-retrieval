3b79ee482cbb257890666bc44021dfec9ccb6653
Nick Belzer <me@nickbelzer.me>
2020-04-10 19:03:03 +0200

    Update word extraction

--- methods/str/extraction/words.py
+++ methods/str/extraction/words.py
@@ -7,7 +7,7 @@ def extract_unique_words(input) -> [str]:
     if type(input) == Table:
         terms = __extract_table_terms(input)
     elif type(input) == Query:
-        terms = input.terms()
+        terms = input.query.split(" ")
     assert terms is not None
     return list(set(terms))
 
@@ -15,4 +15,4 @@ def extract_unique_words(input) -> [str]:
 def __extract_table_terms(table: Table) -> [str]:
     """Uses some aspects of the Table input to retrieve the terms relevant as
     defined by Zhang and Balog."""
-    return [x.split(" ") for x in table.title + table.caption]
+    return " ".join(table.title + [table.caption]).split(" ")
