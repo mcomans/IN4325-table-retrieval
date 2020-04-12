a75c3066f790e011906a127618c743c222987148
Nick Belzer <me@nickbelzer.me>
2020-04-12 21:29:44 +0200

    Add skeleton for bag_of_categories approach

--- methods/str/semantic_matching/bag_of_categories.py
+++ methods/str/semantic_matching/bag_of_categories.py
@@ -1,3 +1,13 @@
+# The Bag Of Categories approach is based on mapping our entities to
+# Wikipedia categories. As it is not explained in the paper by Zhang and
+# Balog we have written our own implementation.
+
 
 def semantic_map(terms: [str]):
+    return [__entity_to_category(term) for term in terms]
+
+
+def __entity_to_category(entity: str) -> [int]:
+    """Maps the entity to a category vector with a 1 for the matching
+    category if one is found."""
     pass
