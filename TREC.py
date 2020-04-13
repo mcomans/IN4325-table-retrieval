ec73162ebaa4e4aa06310da73fb2b458af0bfc92
Daan Oudejans <D.A.J.Oudejans@student.tudelft.nl>
2020-04-13 17:11:19 +0200

    Create results directory if it doesn't exist

--- TREC.py
+++ TREC.py
@@ -1,3 +1,4 @@
+from pathlib import Path
 import time
 
 
@@ -8,6 +9,7 @@ def write_results(results, run_id):
     (query_id, query, table_id).
     :param run_id: An identifier for the run, which will be used as filename and for the STANDARD field in TREC results.
     """
+    Path("results").mkdir(parents=True, exist_ok=True)
     results = results.groupby('query_id').apply(lambda x: x.sort_values(['score'], ascending=False)).reset_index(
         drop=True)
     with open(f'results/{time.strftime("%Y%m%d-%H%M%S")}_{run_id}.txt', 'w') as file:
