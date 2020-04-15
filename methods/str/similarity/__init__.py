6760222ba2f779fee14d7c804d51da752c4f1492
Nick Belzer <me@nickbelzer.me>
2020-04-15 11:11:48 +0200

    Fix incorrect return format on no similarity vectors

--- methods/str/similarity/__init__.py
+++ methods/str/similarity/__init__.py
@@ -12,7 +12,10 @@ def calculate_sim(query_vectors: [[int]],
                   method: SimilarityMethod):
     """Calculate the similarity based on the given method."""
     if len(query_vectors) == 0 or len(table_vectors) == 0:
-        return 0, 0, 0
+        if method == SimilarityMethod.EARLY_FUSION:
+            return 0
+        else:
+            return 0, 0, 0
     if method == SimilarityMethod.EARLY_FUSION:
         return early_fusion.calculate_sim(query_vectors, table_vectors)
     elif method == SimilarityMethod.LATE_FUSION:
